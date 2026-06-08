f79faff520ce1202488ba98c960cecbc7f96942a
Ethan <ethanteb@gmail.com>
2026-06-08 17:01:51 +0200

    url builder function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.data.scraper import fetch_page
+from src.data.scraper import fetch_page, url_builder
 from src.data.parser import parse_stage_results, parse_stage_profile
 
 def test_scrape_gt(url):
@@ -18,7 +18,9 @@ def test_parse_stage_profile(url):
     print(stage_profile)
 
 if __name__ == "__main__":
-    url = "https://www.procyclingstats.com/race/giro-d-italia/2026/stage-15"
+    #url = "https://www.procyclingstats.com/race/giro-d-italia/2026/stage-15"
+    base_url = "https://www.procyclingstats.com/race/"
+    url = url_builder(base_url, "giro", 2026, 21)
 
     #test_scrape_gt(url)
     #test_parse_stage_results(url)
--- src/data/scraper.py
+++ src/data/scraper.py
@@ -14,4 +14,17 @@ def fetch_page(url: str, delay: float = 1.0) -> BeautifulSoup:
     except requests.RequestException as e:
         print(f"Error fetching {url}: {e}")
         return BeautifulSoup("<html><body><h1>Error fetching page</h1></body></html>", 'html.parser')
-
+    
+def url_builder(base_url: str, race: str, year: int, stage: int) -> str:
+    """Constructs the procyclingstats URL for a given race, year, and stage number."""
+    if race.lower() == "giro":
+        full_race_name = "giro-d-italia"
+    elif race.lower() == "tour":
+        full_race_name = "tour-de-france"
+    elif race.lower() == "vuelta":
+        full_race_name = "vuelta-a-espana"
+    else:
+        raise ValueError(f"Unsupported race name: {race}")
+    if not 0 < stage < 22:
+        raise ValueError(f"Invalid stage number: {stage}")
+    return f"{base_url}/{full_race_name}/{year}/stage-{stage}"
