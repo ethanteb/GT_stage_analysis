fdaaa55540d36494c204701da74765c4fa10c102
Ethan <ethanteb@gmail.com>
2026-06-23 15:09:14 +0200

    GT stage length plotting functions

---
+++ gt_stage_analysis/visualisations/visualisations.py
@@ -0,0 +1,90 @@
+#-------------------------------------------------------------------------------------------------------------
+# Imports
+#-------------------------------------------------------------------------------------------------------------
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#-------------------------------------------------------------------------------------------------------------
+# Constants
+#-------------------------------------------------------------------------------------------------------------
+
+GRAND_TOURS: dict[str, str] = {'giro': 'giro-d-italia', 'tour': 'tour-de-france', 'vuelta': 'vuelta-a-espana'}
+COLOURS: dict[str, str] = {'giro': 'hotpink', 'tour': 'gold', 'vuelta': 'red'}
+
+#-------------------------------------------------------------------------------------------------------------
+# Functions to produce a plot showing the length of stages of a single year of all 3 Grand Tours
+#-------------------------------------------------------------------------------------------------------------
+
+def single_year_stage_length(data, year):
+    #Call this function to extract the needed stage length data and the produce the plot
+    if year not in range (data.start_year, data.end_year+1):
+        raise ValueError("Selected year must be in the range of the data.")
+    num_stages_dict = {}
+    for key, value in GRAND_TOURS.items():
+        data_key = f"{key}{year}"
+        num_stages_dict[key] = data.num_stages_dict[data_key]
+    stage_lengths, max_num_stages = extract_length(data, num_stages_dict)
+    plot_stage_lengths(stage_lengths, max_num_stages)
+
+def extract_length(data, num_stages_dict) -> tuple[dict[str, list], int]:
+    '''
+    Extracts the stage length data into three arrays that are returned inside a dictionary with the tour name as key, 
+    the max_num_stages variable that is also returned is the maximum number of stages in any of the grand tours, to be 
+    used in the x-axis length of the plot.
+    '''
+    sum_stages = 0
+    giro_lengths = []
+    tour_lengths = []
+    vuelta_lengths = []
+    for key, value in num_stages_dict.items():
+        sum_stages+=value
+    for num in range(0, sum_stages):
+        if data.list_GT_stages[num].race == 'GIRO':
+            giro_lengths.append(data.list_GT_stages[num].stage_profile.distance_km)
+        elif data.list_GT_stages[num].race == 'TOUR':
+            tour_lengths.append(data.list_GT_stages[num].stage_profile.distance_km)
+        elif data.list_GT_stages[num].race == 'VUELTA':
+            vuelta_lengths.append(data.list_GT_stages[num].stage_profile.distance_km)
+    max_num_stages = max([len(giro_lengths), len(tour_lengths), len(vuelta_lengths)])
+    return {'Giro': giro_lengths, 'Tour': tour_lengths, 'Vuelta': vuelta_lengths}, max_num_stages
+
+def plot_stage_lengths(stage_lengths, max_num_stages):
+    stages = np.arange(1, max_num_stages + 1)
+    fig, ax = plt.subplots(figsize=(13, 5))
+
+    for key, value in stage_lengths.items():
+        ax.plot(stages, value, color=COLOURS[key.lower()], linewidth=1.4, alpha=0.6, zorder=2)
+        ax.scatter(stages, value, label=key, color=COLOURS[key.lower()], s=40, zorder=3,
+                   edgecolors='white', linewidths=0.5)
+
+    ax.yaxis.grid(True, color='lightgrey', linewidth=0.7, zorder=1)
+    ax.set_axisbelow(True)
+    ax.xaxis.grid(False)
+
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_color('lightgrey')
+    ax.spines['bottom'].set_color('lightgrey')
+
+    ax.set_xticks(stages)
+    ax.tick_params(axis='both', labelsize=9, colors='grey')
+    ax.set_xlabel('Stage Number', fontsize=10, labelpad=8, color='black')
+    ax.set_ylabel('Stage Length/km', fontsize=10, labelpad=8, color='black')
+    ax.set_title('Grand Tour Stage Lengths 2025', fontsize=11, color='black')
+
+    legend = ax.legend(
+        title='Grand Tour',
+        title_fontsize=9,
+        fontsize=9,
+        loc='upper left',
+        bbox_to_anchor=(1.01, 1),
+        borderaxespad=0,
+        frameon=True,
+        framealpha=0.9,
+        edgecolor='black',
+    )
+    legend.get_title().set_color('black')
+
+    fig.tight_layout()
+    plt.show()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #-------------------------------------------------------------------------------------------------------------
 
 from gt_stage_analysis.data.data_loading import BASE_URL, GTResults
+from gt_stage_analysis.visualisations.visualisations import single_year_stage_length
 
 #-------------------------------------------------------------------------------------------------------------
 # Main workflow
@@ -13,15 +14,23 @@ if __name__ == "__main__":
     # Scraping & saving to .json file -> comment out when not required
     #---------------------------------------------------------------------------------------------------------
     '''
-    results = GTResults(BASE_URL, 2022, 2022)
+    results = GTResults(BASE_URL, 2025, 2025)
     results.build_stage_list()
     for stage in results.list_GT_stages:
         stage.fetch_page()
         stage.parse_stage_profile()
         stage.parse_stage_results()
-    results.to_json('data/raw/2022_raw')
+    results.to_json('data/raw/2025_raw')
     '''
     #---------------------------------------------------------------------------------------------------------
     # Loading from .json -> comment out when not required
     #---------------------------------------------------------------------------------------------------------
-    results = GTResults.load_json('data/raw/2022_raw')
+    
+    results = GTResults.load_json('data/raw/2025_raw')
+
+    #---------------------------------------------------------------------------------------------------------
+    # Simple stage length plot -> setup to produce a plot showing the stage lengths for 1 year of GTs
+    #---------------------------------------------------------------------------------------------------------
+
+    single_year_stage_length(results, 2025)
+    
