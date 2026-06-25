c7645f7398ef9d4d43a10a849feacd4102db3cea
Ethan <ethanteb@gmail.com>
2026-06-25 17:35:10 +0200

    updated data

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ if __name__ == "__main__":
     # Scraping & saving to .json file -> comment out when not required
     #---------------------------------------------------------------------------------------------------------
     '''
-    results = GTResults(base_url=BASE_URL, start_year=2010, end_year=2025)
+    results = GTResults(base_url=BASE_URL, start_year=2020, end_year=2025)
     results.build_stage_list()
     for stage in results.list_GT_stages:
         stage.fetch_page()
         stage.parse_stage_profile()
         stage.parse_stage_results()
-    results.to_json(loc='data/raw/2010_2025_raw')
+    results.to_json(loc='data/raw/2020_2025_raw')
     '''
     #---------------------------------------------------------------------------------------------------------
     # Loading from .json -> comment out when not required
     #---------------------------------------------------------------------------------------------------------
     
-    results = GTResults.load_json(loc='data/raw/2010_2025_raw')
+    results = GTResults.load_json(loc='data/raw/2020_2025_raw')
     
     #---------------------------------------------------------------------------------------------------------
     # Simple stage length plot -> setup to produce a plot showing the stage lengths for 1 year of GTs
@@ -41,3 +41,4 @@ if __name__ == "__main__":
     
     df = results.convert_to_breakaway_dataframe()
     breakaway_success_plot(df)
+    
